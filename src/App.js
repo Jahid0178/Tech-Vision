b1e751ee8632076dd15c75333ed787891e77d81c
Jahid <jahidulhaque767@gmail.com>
2021-12-17 04:18:30 +0600

    added google login

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import "./App.css";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
 import Footer from "./components/Shared/Footer/Footer";
 import Navigation from "./components/Shared/Navigation/Navigation";
+import AuthProvider from "./contexts/AuthProvider/AuthProvider";
 import Home from "./Pages/Home";
 import Shop from "./Pages/Shop";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Navigation></Navigation>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/view-details:productId" element={<ProductDetails />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navigation></Navigation>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route
+              path="/view-details:productId"
+              element={<ProductDetails />}
+            />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,33 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
+import initializeFirebase from "../components/Login/Firebase/firebase.init";
+
+initializeFirebase();
+const useFirebase = () => {
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
+
+  const auth = getAuth();
+
+  // Provider
+  const googleProvider = GoogleAuthProvider();
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        setError(errorMessage);
+      });
+  };
+  return {
+    user,
+    error,
+    signInWithGoogle,
+  };
+};
+
+export default useFirebase;
