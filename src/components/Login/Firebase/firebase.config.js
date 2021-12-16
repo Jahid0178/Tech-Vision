f363d9a16e896289516f80ef8a7b32602f8c4ff2
Jahid <jahidulhaque767@gmail.com>
2021-12-17 03:55:34 +0600

    added firebase initialize or firebase config code

---
+++ src/components/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAx1m6YlH6hU3Ghb10HNcEXezYAmgoZRsg",
+  authDomain: "tech-vision-2cf9f.firebaseapp.com",
+  projectId: "tech-vision-2cf9f",
+  storageBucket: "tech-vision-2cf9f.appspot.com",
+  messagingSenderId: "3032868820",
+  appId: "1:3032868820:web:a6efd65f91bf1d416e1841",
+};
+
+export default firebaseConfig;
