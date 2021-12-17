02c920a8047467dfb7204575175840f6fe4f3e29
Jahid <jahidulhaque767@gmail.com>
2021-12-18 05:09:19 +0600

    added graphics card on home page

--- src/components/Home/Graphics/Graphics.js
+++ src/components/Home/Graphics/Graphics.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
+import { Swiper, SwiperSlide } from "swiper/react";
+import GraphicsCard from "../GraphicsCard/GraphicsCard";
 import "./Graphics.css";
 
 const Graphics = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const url = "https://tech-vision-dev.herokuapp.com/products";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) =>
+        setProducts(
+          data.filter((product) => product.category === "graphics-card")
+        )
+      );
+  }, []);
+
   return (
     <Container>
-      <h2 className="title-underline">Graphics</h2>
+      <h2 className="title-underline">Graphics Card</h2>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        className="mySwiper"
+      >
+        {products.map((product) => (
+          <SwiperSlide key={product._id}>
+            <GraphicsCard product={product} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </Container>
   );
 };
---
+++ src/components/Home/GraphicsCard/GraphicsCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./GraphicsCard.css";
+
+const GraphicsCard = (props) => {
+  const { _id, name, smallImg } = props.product;
+  const navigate = useNavigate();
+  const viewDetails = (id) => {
+    navigate(`/productDetails/${id}`);
+  };
+  return (
+    <div className="my-3">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={smallImg} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button onClick={() => viewDetails(_id)} variant="primary">
+            View Details
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default GraphicsCard;
